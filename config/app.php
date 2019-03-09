884a2a02405cfe21595eb0bc5c76a3041f055aaa
joseluir <riveraibarra.solutions@gmail.com>
2019-03-09 13:02:28 -0500

    fix error

--- config/app.php
+++ config/app.php
@@ -210,7 +210,7 @@ return [
         'Hash' => Illuminate\Support\Facades\Hash::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
-        'Mail' => Illuminate\Support\Facades\Mail::class,git
+        'Mail' => Illuminate\Support\Facades\Mail::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
