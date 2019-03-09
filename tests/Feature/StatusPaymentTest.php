36d88bbcc47a2e6f870aa837a2c7a578917d11ff
joseluir <riveraibarra.solutions@gmail.com>
2019-03-09 15:21:22 -0500

    test of payment status

---
+++ tests/Feature/StatusPaymentTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class StatusPaymentTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testGetStatus()
+    {
+        $data = [
+            'requesId'  => 121212
+        ];
+
+        $rsp = $this->json('POST','/api/v1/pay/query/status',$data);
+
+        $rsp->assertStatus(200);
+
+        $rsp ->assertJson([
+            'success' => true,
+            'error' => []
+        ]);
+
+        $rsp ->assertJsonStructure([
+            'data',
+            'success',
+            'error'
+        ]);
+
+    }
+}
